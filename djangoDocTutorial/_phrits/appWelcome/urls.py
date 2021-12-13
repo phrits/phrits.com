def5f30f81aeed5262c44a4476008467c2fb6d78
Fritz Knack <60327058+phrits@users.noreply.github.com>
2021-12-13 16:19:04 -0500

    url -> view and template bound

--- djangoDocTutorial/_phrits/appWelcome/urls.py
+++ djangoDocTutorial/_phrits/appWelcome/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+
+"""
+'index' is also returned for '' in the project urls.py.
+"""
 urlpatterns = [
     path('', views.index, name='index'),
+    
+# *****************************************************************************
+
+    # ex: /appWelcome/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /appWelcome/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /appWelcome/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- djangoDocTutorial/_phrits/appWelcome/views.py
+++ djangoDocTutorial/_phrits/appWelcome/views.py
@@ -1,6 +1,35 @@
+"""
+Templates:  from django.shortcuts import render
+Send:       from django.http import HttpResponse
+
+Default home page for appWelcome.
+
+It's also the home page for the site.
+    _phrits/urls.file calls:
+        path('', include('appWelcome.urls')),
+in its urlpatterns.
+
+def index(request):
+    return HttpResponse("You're at the appWelcome index.")
+"""
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# **** Tutorial
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the appWelcome index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
