48d2c9cdec246ca09759cc78ad9ecd889413d047
Fritz Knack <60327058+phrits@users.noreply.github.com>
2021-12-15 16:23:29 -0500

    reorged settings.py, added about.html; 'welcome' isn't unique

--- _phrits/_phrits/urls.py
+++ _phrits/_phrits/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('welcome.urls')),
+    path('welcome/', include('welcome.urls')),
+    re_path(r'(?i)^about/', include('welcome.urls')),
+    path('admin/', admin.site.urls),
 ]
--- _phrits/welcome/urls.py
+++ _phrits/welcome/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import re_path, path
 
 from . import views
 
 app_name = 'welcome'
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'(?i)^about/.*$', views.about, name='about'),
 ]
