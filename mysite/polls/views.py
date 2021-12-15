af858585b97bfbfc96ec7fc8ae766de90f14fc6d
Fritz Knack <60327058+phrits@users.noreply.github.com>
2021-12-15 13:42:23 -0500

    part 5 fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,10 +24,9 @@ class IndexView(generic.ListView):
 # Detail
 # *****************************************************************************
 class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 # *****************************************************************************
