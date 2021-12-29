1ad6560b3f29cdb9dbca8a12a7e4f4ae26970d1c
Fritz Knack <60327058+phrits@users.noreply.github.com>
2021-12-28 23:58:24 -0500

    wip

---
+++ scrape_old_site/scrape_old_site/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scrape_old_site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrape_old_site.settings')
+
+application = get_asgi_application()
---
+++ scrape_old_site/scrape_old_site/settings.py
@@ -0,0 +1,76 @@
+"""
+Django settings for scrape_old_site project.
+"""
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# ****************
+# High Level Stuff
+DEBUG = True
+STATIC_URL = 'static/'
+ROOT_URLCONF = 'scrape_old_site.urls'
+WSGI_APPLICATION = 'scrape_old_site.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ********
+# SECURITY
+SECRET_KEY = 'django-insecure-q2c@7g2p425697i@_xt!+=!ke-+glavybwe@f%jhfr0&41vr!t'
+ALLOWED_HOSTS = []
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+# **********************
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# ********************
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'EST5EDT'
+USE_I18N = True
+USE_TZ = True
