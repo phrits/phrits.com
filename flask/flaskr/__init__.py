93f0dd320c091f9746a03cdcc8e9976e9536daea
Fritz Knack <60327058+phrits@users.noreply.github.com>
2022-03-22 22:30:45 -0400

    wip

--- flask/flaskr/__init__.py
+++ flask/flaskr/__init__.py
@@ -1,6 +1,13 @@
 import os
 
 from flask import Flask
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
 
 
 def create_app(test_config=None):
@@ -30,21 +37,27 @@ def create_app(test_config=None):
     def hello():
         return "Hello, World!"
 
-    # register the database commands
-    from flaskr import db
+    @app.route("/fattom")
+    def fattom():
+        return render_template("food/fattom.html")
 
-    db.init_app(app)
+
+
+    # register the database commands
+#    from flaskr import db
+
+#    db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog
+#    from flaskr import auth, blog
 
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+#    app.register_blueprint(auth.bp)
+#    app.register_blueprint(blog.bp)
     
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
     # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
+#    app.add_url_rule("/", endpoint="index")
 
     return app
